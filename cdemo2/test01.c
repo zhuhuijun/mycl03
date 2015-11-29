bedd5a605c81bbe503652465115ae4b95597cc0e
zhuhuijun <zhuhuijun@mofangge.com>
2015-11-29 10:56:17 +0800

    90

--- cdemo2/test01.c
+++ cdemo2/test01.c
@@ -16,21 +16,32 @@ int mem_init(int **p)
 	*p=malloc(100);
 	return 0;
 }
+void freemem(int **p){
+	if(*p==NULL)
+	{
+		return;
+	}
+	free(*p);
+	*p=NULL;
+}
 //////////////////////////////////////////////////////////////////////////
 void main()
 {
 	int tmp=0;
 	int *buf;
 	int ren=0;
+
 	change(&tmp);
 	printf("tmp:%d\n",tmp);
 	printf("Hello,world!\n");
+
 	{
 		printf("tmp:%d\n",ren);
-		ren=mem_init(&buf);
+		ren=mem_init(&buf);//为指针分配内存地址
 		printf("buf:%d\n",&tmp);
 		*buf=123;
 		printf("buf:%d\n",*buf);
 	}
+	freemem(&buf);
 	system("pause");
 }
