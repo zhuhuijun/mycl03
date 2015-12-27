221b59486d3d855711cf5c362ebef654cda76efa
unknown <zhuhjtcx@qq.com>
2015-12-27 21:00:18 +0800

    数组

--- cdemo2/day04/shuzu01.c
+++ cdemo2/day04/shuzu01.c
@@ -3,7 +3,7 @@
 #include<stdio.h>
 #include<string.h>
 
-void main()
+void mainbkjl()
 {
 	int a[]={1,2};//隐式指定长度
 	int b[10]={1,2};//显示指定长度
@@ -13,14 +13,32 @@ void main()
 	//c是数组首元素的地址//c+1 步长 int 4
 	//&c是整个数组的地址//&c+1   100*4 400
 	//数组首元素的地址和数组的地址值相等
+	//固定大小内存块的别名
 	printf("%d\n",c);
-
-
-	
-	
-	
 	printf("%d\n",&c);
 	printf("%s\n","hello,world!");
 	system("pause");
 
+}
+//怎样表达int a[10]这种数据类型
+//类型本职:固定大小内存块的别名
+//定义数组类型
+void mainjfaljgalg(){
+	int i=0;
+	typedef int (MyArrayType)[10];//定义了数据类型是数组数据类型
+	MyArrayType myarr;//int myarr[5];
+	for (i=0;i<5;i++)
+	{
+		myarr[i]=i+1;
+	}
+	for (i=0;i<5;i++)
+	{
+		printf("%d\n",myarr[i]);
+	}
+	printf("myarr代表数组首元素地址:%d\n",myarr);
+	printf("myarr+1步长为4:%d\n",myarr+1);
+	printf("&myarr代表数组地址:%d\n",&myarr);
+	printf("&myarr+1步长为20:%d\n",&myarr+1);
+	printf("%s\n","hello,world!");
+	system("pause");
 }
---
+++ cdemo2/day04/shuzu02.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+//////////////////////////////////////////////////////////////////////////
+//数组类型指针：数组指针用于指向数组
+void main()
+{
+	char *myarrk[]={"111111","22222","33333"};//指针数组
+	//数组指针：一个数组指向指针
+	int i=0;
+	typedef int (MyArrayType)[5];//定义了数据类型是数组数据类型
+	MyArrayType myarr;//int myarr[5];//类型定义变量
+	MyArrayType *pArray;//定义一个指针变量  这个指针变量指向一个数组
+	{
+		int a;
+		int *p;
+		p=&a;
+	}
+	{
+		//pArray=&myarr;
+		int yArr2[5];//相当于一级指针
+		pArray=&yArr2;//相当于二级指针
+		for (i=0;i<5;i++)
+		{
+			//yArr2[i]=i+1;
+			(*pArray)[i]=i+1;
+		}
+
+		for (i=0;i<5;i++)
+		{
+			printf("%d\n",(*pArray)[i]);	
+		}
+	
+	}
+	printf("%s\n","hello,world!");
+	system("pause");
+
+}
