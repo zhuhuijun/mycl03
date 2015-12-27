6f3b581feb10bcb52dab902cc2641eee0d3ddf05
unknown <zhuhjtcx@qq.com>
2015-12-27 21:08:00 +0800

    kl

---
+++ cdemo2/day04/shuzu03.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+//////////////////////////////////////////////////////////////////////////
+//定义数组指针变量的第三种写法
+void main()
+{
+	printf("%s\n","hello,world!");
+	system("pause");
+
+}
